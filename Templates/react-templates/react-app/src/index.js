c3f4c8196bfa02b07b938afb6b6153dc07331750
siva-piskool <siva.piskool@gmail.com>
2020-01-21 15:41:15 +0530

    React from scratch created

---
+++ Templates/react-templates/react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+  return <div style={{ color: "red" }}>Welcome to React App</div>;
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
